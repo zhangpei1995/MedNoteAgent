e49591cb850b7c38d4d09d421087270d0fa4b9e0
zhangpei <zhangpeibisha@163.com>
2026-06-10 21:43:07 +0800

    1

---
+++ src/main/java/org/med/note/MedNoteAgentApplication.java
@@ -0,0 +1,10 @@
+package org.med.note;
+
+/**
+ * Hello world!
+ */
+public class MedNoteAgentApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
